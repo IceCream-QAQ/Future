f82754bac1e660f90cc079ab47600d67eca3d342
IceCream <www@withdata.net>
2022-01-24 02:57:45 +0800

    readme

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "1.6.10"
+}
+
+group = "com.IceCreamQAQ"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
